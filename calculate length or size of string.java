6a8cfeaaaec09df54a6fc1d460e0b024f81a5bd7
Ananya <100693194+Anu243@users.noreply.github.com>
2022-03-29 22:29:23 +0530

    Create calculate length or size of string.java

---
+++ calculate length or size of string.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Length{
+    public static void main(String[] args) {
+        String str;
+       
+        System.out.println("enter the string");
+        Scanner sc=new Scanner(System.in);
+        str=sc.nextLine();
+        
+            System.out.println("string length is "+str.length());
+    }
+}

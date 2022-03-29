55db730d7086510ded8c70ad842d2779f0b3262b
Ananya <100693194+Anu243@users.noreply.github.com>
2022-03-29 22:25:35 +0530

    Create prove string is immutable.java

---
+++ prove string is immutable.java
@@ -0,0 +1,8 @@
+class StringImmutable{
+    public static void main(String[] args) {
+        String s="JAVA";  
+         System.out.println(s);
+         s.concat("PROGRAMMING");  
+         System.out.println(s);
+ }  
+}  

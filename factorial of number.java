2abe56ef24e7b00d5e9c11adc2901c28865eba3a
Ananya <100693194+Anu243@users.noreply.github.com>
2022-03-31 01:32:02 +0530

    Create factorial of number.java

---
+++ factorial of number.java
@@ -0,0 +1,20 @@
+/*Write a program to find the factorial of a given number.*/
+
+import java.util.Scanner;
+public class Main{
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+	
+		System.out.printf("Enter value ");
+	int n= sc.nextInt(); 
+int i,fact=1;  
+      
+  for(i=1;i<=n;i++){    
+      fact=fact*i;    
+  }    
+  System.out.println(fact);    
+		
+	}
+}

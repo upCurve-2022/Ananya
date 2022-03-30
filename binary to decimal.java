f402635b69d26ad612828c372ba7dae028444514
Ananya <100693194+Anu243@users.noreply.github.com>
2022-03-31 01:08:05 +0530

    Create binary to decimal.java

---
+++ binary to decimal.java
@@ -0,0 +1,26 @@
+*/Write a program to convert binary to decimal.
+Input: 1100100
+Output: 100
+*/
+ import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+
+        System.out.println("Enter  number:");
+        int n=s.nextInt();
+
+        int ans=0,p=0;
+
+        while(n!=0)
+        {
+            ans+=((n%10)*Math.pow(2,p));
+            n=n/10;
+            p++;
+        }
+
+        System.out.println(ans);
+    }
+}

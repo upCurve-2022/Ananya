dec4be344456a5b56cb82e5fd11ecb77d1cd9b45
Ananya <100693194+Anu243@users.noreply.github.com>
2022-03-31 02:15:56 +0530

    Create perfect square.java

---
+++ perfect square.java
@@ -0,0 +1,23 @@
+//Write a java program to check if given number is a perfect square.
+
+import java.util.Scanner;  
+public class CheckPerfectSquare
+{   
+ 
+static boolean checkPerfectSquare(double number)    
+{   
+double sqrt=Math.sqrt(number);   
+return ((sqrt - Math.floor(sqrt)) == 0);   
+}   
+
+public static void main(String[] args)    
+{   
+System.out.print("Enter any number: ");
+Scanner sc=new Scanner(System.in);    
+double number=sc.nextDouble();   
+if (checkPerfectSquare(number))   
+System.out.print("the given number is perfect square.");   
+else  
+System.out.print("the given number is not perfect square.");   
+}   
+}   

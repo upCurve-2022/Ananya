0af806d089c9e51c2dd88986d073b8af3ebff312
Ananya <100693194+Anu243@users.noreply.github.com>
2022-03-31 01:40:00 +0530

    Create remove all occurences of a character in string.java

---
+++ remove all occurences of a character in string.java
@@ -0,0 +1,35 @@
+// Write a program to remove all occurrences of a given character from input String:
+//Given string : To test this string >> remove all ‘t/T’ from this string and print .
+
+import java.util.Arrays;
+import java.util.List;
+
+class Test
+{
+	static String remVowel(String str)
+	{
+		Character vowels[] = {'t', 'T'};
+		
+		List<Character> al = Arrays.asList(vowels);
+		
+		StringBuffer sb = new StringBuffer(str);
+		
+		for (int i = 0; i < sb.length(); i++) {
+			
+			if(al.contains(sb.charAt(i))){
+				sb.replace(i, i+1, "") ;
+				i--;
+			}
+		}
+		
+		
+		return sb.toString();
+	}
+	// Driver method to test the above function
+	public static void main(String[] args)
+	{
+		String str = "To test this string";
+		
+		System.out.println(remVowel(str));
+	}
+}

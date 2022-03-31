59f3d21a7ef25c54f1f39ddb8e6a618462724e8a
Ananya <100693194+Anu243@users.noreply.github.com>
2022-03-31 23:57:25 +0530

    Create replace ‘a’ with ‘$’ in given String .java

---
+++ replace ‘a’ with ‘$’ in given String .java
@@ -0,0 +1,10 @@
+// Write a java program to replace ‘a’ with ‘$’ in the following String ....“I am always ready to learn although I do not always like being taught.”
+
+public class Main {
+   public static void main(String[] args) {
+      String str1 = "I am always ready to learn although I do not always like being taught.";
+      System.out.println("String: "+str1);
+      String str2 = str1.replace('a', '$');  
+      System.out.println("Updated string: "+str2);
+   }
+}

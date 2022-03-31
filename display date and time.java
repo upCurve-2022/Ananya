74627613087b831bf1bd9737b2f90ee745493683
Ananya <100693194+Anu243@users.noreply.github.com>
2022-03-31 23:04:58 +0530

    Create display date and time.java

---
+++ display date and time.java
@@ -0,0 +1,11 @@
+//Write a java program to display current Date and Time.
+import java.util.*;
+ 
+public class Main {
+    public static void main(String args[])
+    {
+        Date current_Date = new Date();
+        System.out.println(current_Date);
+        // print the time and date
+    }
+}

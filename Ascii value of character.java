a262df9c65cb3aa7b3e2fbede5ebcf3a1621c9b3
Ananya <100693194+Anu243@users.noreply.github.com>
2022-03-31 23:06:28 +0530

    Create Ascii value of character.java

---
+++ Ascii value of character.java
@@ -0,0 +1,15 @@
+//Write a java program to find the ASCII value of the character.
+
+public class AsciiValue {
+
+    public static void main(String[] args) {
+
+        char ch = 'a';
+        int ascii = ch;
+        // You can also cast char to int
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+}
